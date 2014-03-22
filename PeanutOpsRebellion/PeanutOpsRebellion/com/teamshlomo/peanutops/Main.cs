a221fa73e0447139abf6e85227b7d1fb5cb2976a
Richard Hogans <greenshellservers@gmail.com>
2014-03-22 11:42:43 -0400

    Initial Commit
    There is nothing wrong here.

---
+++ PeanutOpsRebellion/PeanutOpsRebellion/com/teamshlomo/peanutops/Main.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeanutOpsRebellion
+{
+    class Main
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Peanut Ops");
+            Console.WriteLine("Rebellion");
+        }
+    }
+}
